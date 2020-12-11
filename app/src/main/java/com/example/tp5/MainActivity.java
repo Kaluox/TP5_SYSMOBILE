e87a038f4b94490ba69e2e3f89fe9ef9690f7e82
Kaluox <alexis-marlin@orange.fr>
2020-12-12 00:01:04 +0100

    tp5 first try

--- app/src/main/java/com/example/tp5/MainActivity.java
+++ app/src/main/java/com/example/tp5/MainActivity.java
@@ -8,10 +8,19 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.StrictMode;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,38 +28,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        MainActivity lecontext = this;
+        // Titre
+        lecontext.setTitle("test");
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.textView);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+            public void onClick(View v) {
+                URL url;
+                HttpURLConnection urlConnection = null;
+                try {
+                    url = new URL("http://www.google.com/");
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    readStream(in);
+                    urlConnection.disconnect();
+                }catch (Exception e){
+                    // TODO Auto generated catch block
+                    e.printStackTrace();
+                    urlConnection.disconnect();}
+                }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void readStream(InputStream in) {
+        TextView tv = findViewById(R.id.textView);
+        try {
+            int x = in.read();
+            tv.setText(x);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
