30d2a4238aa791216650d727653ad529156b0d49
Victor <victor.chrzanowski@etu.univ-tours.fr>
2020-12-12 17:45:40 +0100

    commit parse XML

--- app/src/main/java/com/example/tp5/CallWebApi.java
+++ app/src/main/java/com/example/tp5/CallWebApi.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -43,6 +47,36 @@ public class CallWebApi extends AsyncTask<String, String, String> {
 
         }
         return "error";
+    }
+    private GeoIP parseXML( XmlPullParser parser ) throws XmlPullParserException, IOException {
+        int eventType = parser.getEventType();
+        GeoIP result = new GeoIP();
+        while( eventType!= XmlPullParser.END_DOCUMENT) {
+            String name = null;
+            switch(eventType)
+            {
+                case XmlPullParser.START_TAG:
+                name = parser.getName();
+                if( name.equals("Error")) {
+                    System.out.println("Web API Error!");
+                }
+                else if( name.equals("countryCode")) {
+                    result.countryCode=parser.nextText();
+                }
+                else if(name.equals("query")) {
+                    result.query= parser.nextText();
+                }
+                else if(name.equals("country")) {
+                    result.country= parser.nextText();
+                }
+                //ajouter d'autres champs
+                break;
+                case XmlPullParser.END_TAG:
+                    break;
+            } // end switch
+            eventType = parser.next();
+        } // end while
+        return result;
     }
 
     protected void onPostExecute(String result){
